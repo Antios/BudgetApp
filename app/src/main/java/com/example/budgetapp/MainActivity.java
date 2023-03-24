3658f8240f41e759ef89fefd60340f07f87ea14a
Matthew Comeau <micomeau@yahoo.com>
2023-03-24 13:37:07 -0300

    added some new UI parts

--- app/src/main/java/com/example/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/budgetapp/MainActivity.java
@@ -9,6 +9,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Create an instance of the HomeScreenView class
+        HomeScreenView homeView = new HomeScreenView(this);
+
+        // Set the login screen view as the content view for the activity
+        setContentView(homeView);
     }
 }
