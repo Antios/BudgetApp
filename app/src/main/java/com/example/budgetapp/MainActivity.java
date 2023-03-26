40b72cc25428cd5cb13bfd5ca6fa4204a871db6a
Matthew Comeau <micomeau@yahoo.com>
2023-03-26 13:34:09 -0300

    finished nav swap

--- app/src/main/java/com/example/budgetapp/MainActivity.java
+++ app/src/main/java/com/example/budgetapp/MainActivity.java
@@ -1,19 +1,45 @@
 package com.example.budgetapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.transaction_view);
 
-        // Create an instance of the HomeScreenView class
-        HomeScreenView homeView = new HomeScreenView(this);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new TransactionView()).commit();
 
-        // Set the login screen view as the content view for the activity
-        setContentView(homeView);
+        bottomNavigationView.setSelectedItemId(R.id.new_transactions_nav);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId()) {
+                    case R.id.new_transactions_nav:
+                        fragment = new TransactionView();
+                        break;
+                    case R.id.balance_nav:
+                        fragment = new BalanceView();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_container, fragment).commit();
+
+                return true;
+            }
+
+        });
     }
 }
